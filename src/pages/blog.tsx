713afc1b35f70419bfa29a131ec4ed082cd81a8d
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2021-04-22 13:22:54 -0700

    changed about page for private route

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,7 +1,23 @@
 import { server } from '../config'
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/client'
 import ArticleList from '../components/ArticleList'
+import { useEffect } from 'react'
 
 export default function Home({ articles }) {
+  const router = useRouter()
+  const [session] = useSession();
+
+  useEffect(() => {
+    !session && router.push('/')
+  }, [])
+
+  if(!session) {
+    return (
+      <div><p>loading...</p></div>
+    )
+  }
+
   return (
     <div>
       <ArticleList articles={articles} />
